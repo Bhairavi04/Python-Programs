25d4c6965d015a5ca60f24701411bb5f242a224f
Bhairavi <bhairavishewde04@gmail.com>
2022-07-26 01:54:28 +0530

    Add files via upload

---
+++ integerssums.py
@@ -0,0 +1,13 @@
+# Write a function, add_it_up(),
+# that takes a single integer as input and returns the sum of the integers from zero to the input parameter.
+
+
+def add_it_up(a):
+    """This function takes a single input integer and returns the sum of the integers from zero to the input parameter"""
+    i = 0
+    for b in range(0, a+1):
+        i = i+b
+    print(i)
+
+
+add_it_up(4)
