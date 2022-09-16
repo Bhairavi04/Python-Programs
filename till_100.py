425a48cc0f44646c17f0d98f6595d6f814b09396
Bhairavi <bhairavishewde04@gmail.com>
2022-09-16 22:16:47 +0530

    Add files via upload

---
+++ till_100.py
@@ -0,0 +1,11 @@
+#Keep printing till the user enters number less than or equal to 100.Once the entered number is greater than 100 print congrats you have printed till 100.
+while (True):
+    number=int(input("Enter the number : "))
+    if number <=100:
+        continue
+    if number>100:
+        print("congrats")
+        break
+
+
+    
