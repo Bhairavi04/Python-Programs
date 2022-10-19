3836a3f5ff26efb69f5283b2f5489583eae4e71c
Bhairavi <bhairavishewde04@gmail.com>
2022-10-19 20:08:48 +0530

    Add files via upload

---
+++ chars_at_even_index.py
@@ -0,0 +1,6 @@
+# Write a program to accept a string from the user and display characters that are present at an even index number.
+
+
+str = input("Enter the string :")
+for i in range(0, len(str), 2):
+    print(str[i])
